d2e9f4d8bf10ab2c4416b653596ed7fbc35981a7
trungle <trung.le@seldatinc.com>
2018-03-17 00:14:02 +0700

    update

--- src/app/pages/layout/menu/menu.component.ts
+++ src/app/pages/layout/menu/menu.component.ts
@@ -7,26 +7,62 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+    MENU_CONSTANT = [];
 
     showMenu: string = '';
-    pushRightClass: string = 'push-right';
+
 
     constructor(private translate: TranslateService, public router: Router) {
-        this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de']);
-        this.translate.setDefaultLang('en');
-        const browserLang = this.translate.getBrowserLang();
-        this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de/) ? browserLang : 'en');
 
-        this.router.events.subscribe(val => {
-            if (
-                val instanceof NavigationEnd &&
-                window.innerWidth <= 992 &&
-                this.isToggled()
-            ) {
-                this.toggleSidebar();
+    }
+
+    ngOnInit() {
+        this.MENU_CONSTANT = [
+            {
+                flag: '',
+                link: '/home',
+                main_name: 'Home',
+                icon: 'fa fa-home',
+                sub: false,
+                child: []
+            },
+            {
+                flag: 'dashboard',
+                link: '/',
+                main_name: 'Dashboard',
+                icon: '',
+                sub: true,
+                child: [
+                    {
+                        link: '/dashboard',
+                        name: 'Promotion Dashboard'
+                    },
+                    {
+                        link: '/blank-pag',
+                        name: 'Overall Dashboard'
+                    }
+                ]
+            },
+            {
+                flag: 'purchase',
+                link: '/',
+                main_name: 'Purchasing Management',
+                icon: '',
+                sub: true,
+                child: [
+                    {
+                        link: '/dashboards',
+                        name: 'Purchase Order'
+                    },
+                    {
+                        link: '/blank-page',
+                        name: 'Purchase Quotation'
+                    }
+                ]
             }
-        });
+        ]
     }
 
 
@@ -38,27 +74,5 @@ export class MenuComponent {
         }
     }
 
-    isToggled(): boolean {
-        const dom: Element = document.querySelector('body');
-        return dom.classList.contains(this.pushRightClass);
-    }
-
-    toggleSidebar() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle(this.pushRightClass);
-    }
-
-    rltAndLtr() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle('rtl');
-    }
-
-    changeLang(language: string) {
-        this.translate.use(language);
-    }
-
-    onLoggedout() {
-        localStorage.removeItem('isLoggedin');
-    }
 
 }
