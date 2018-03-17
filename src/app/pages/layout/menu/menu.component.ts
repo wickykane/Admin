b17ccf79fe6124b8646a82f0c435b29205c7647d
trungle <trung.le@seldatinc.com>
2018-03-17 11:39:21 +0700

    update

--- src/app/pages/layout/menu/menu.component.ts
+++ src/app/pages/layout/menu/menu.component.ts
@@ -23,7 +23,7 @@ export class MenuComponent implements OnInit {
             {
                 flag: '',
                 link: '/home',
-                main_name: 'Home',
+                main_name: '',
                 icon: 'fa fa-home',
                 sub: false,
                 child: []
@@ -54,13 +54,158 @@ export class MenuComponent implements OnInit {
                 child: [
                     {
                         link: '/dashboards',
-                        name: 'Purchase Order'
+                        name: 'Purchase Quotation'
                     },
                     {
-                        link: '/blank-page',
-                        name: 'Purchase Quotation'
+                        link: '/sd',
+                        name: 'Purchase Orders'
+                    },
+                    {
+                        link: '/d-page',
+                        name: 'Inbound Delivery Orders'
+                    },
+                    {
+                        link: '/asdf',
+                        name: 'Warehouse Receipt'
+                    },
+                    {
+                        link: '/233',
+                        name: 'Suppliers'
                     }
                 ]
+            },
+            {
+                flag: 'sale',
+                link: '/',
+                main_name: 'Sales Management',
+                icon: '',
+                sub: true,
+                child: [
+                    {
+                        link: '/swa',
+                        name: 'Buyer RFQ'
+                    },
+                    {
+                        link: '/sdw',
+                        name: 'Sales Quotation'
+                    },
+                    {
+                        link: '/dd',
+                        name: 'Sales Orders'
+                    },
+                    {
+                        link: '/asdf',
+                        name: 'Delivery Orders'
+                    },
+                    {
+                        link: '/23s3',
+                        name: 'Sales Price List'
+                    }
+                ]
+            },
+            {
+                flag: 'promotion',
+                link: '/',
+                main_name: 'Promotion Management',
+                icon: '',
+                sub: true,
+                child: [
+                    {
+                        link: '/swaa',
+                        name: 'Promotion Budget'
+                    },
+                    {
+                        link: '/sdsw',
+                        name: 'Promotion Campaign'
+                    }
+                ]
+            },
+            {
+                flag: 'customer',
+                link: '/',
+                main_name: 'Customers Management',
+                icon: '',
+                sub: true,
+                child: [
+                    {
+                        link: '/saswaa',
+                        name: 'Buyers'
+                    },
+                    {
+                        link: '/sd',
+                        name: 'Customer Segments'
+                    }
+                ]
+            },
+            {
+                flag: 'product',
+                link: '/',
+                main_name: 'Products Management',
+                icon: '',
+                sub: true,
+                child: [
+                    {
+                        link: '/s2wa',
+                        name: 'Item List '
+                    },
+                    {
+                        link: '/s3dw',
+                        name: 'Product Definition'
+                    },
+                    {
+                        link: '/d3d',
+                        name: 'Bundle Management'
+                    },
+                    {
+                        link: '/asd3f',
+                        name: 'Conditional Product Groups'
+                    },
+                    {
+                        link: '/233s3',
+                        name: 'E-Catalog'
+                    }
+                ]
+            },
+            {
+                flag: 'delivery',
+                link: '/',
+                main_name: 'Delivery Management',
+                icon: '',
+                sub: true,
+                child: [
+                    {
+                        link: '/sa2wa',
+                        name: 'Dashboard'
+                    },
+                    {
+                        link: '/s3sdw',
+                        name: 'Delivery Order'
+                    },
+                    {
+                        link: '/d3ds',
+                        name: 'Routes'
+                    },
+                    {
+                        link: '/assd3f',
+                        name: 'Trucks   '
+                    },
+                    {
+                        link: '/23s3s3',
+                        name: 'Truck Types'
+                    },
+                    {
+                        link: '/s',
+                        name: 'Drivers'
+                    }
+                ]
+            },
+            {
+                flag: '',
+                link: '/d',
+                main_name: 'Admin Panel',
+                icon: '',
+                sub: false,
+                child: []
             }
         ]
     }
