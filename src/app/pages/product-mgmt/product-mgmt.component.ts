c63956decf4fc3f3053b78fb3565c2114ed9a829
Khoa To <khoa.to@seldatinc.com>
2018-03-22 14:56:55 +0700

    update

---
+++ src/app/pages/product-mgmt/product-mgmt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-mgmt',
+  templateUrl: './product-mgmt.component.html',
+  styleUrls: ['./product-mgmt.component.scss']
+})
+export class ProductMgmtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/product-mgmt/product-mgmt.module.ts
+++ src/app/pages/product-mgmt/product-mgmt.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BundleMgmtComponent } from './bundle-mgmt/bundle-mgmt.component';
-import { ConditionProductGroupComponent } from './condition-product-group/condition-product-group.component';
-import { ECatalogComponent } from './e-catalog/e-catalog.component';
-import { ItemListComponent } from './item-list/item-list.component';
-import { ProductDefinitionComponent } from './product-definition/product-definition.component';
+
+import { ProductMgmtRoutingModule } from './product-mgmt-routing.module';
+import { ProductMgmtComponent } from './product-mgmt.component';
+import {ItemListComponent} from './item-list/item-list.component';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ProductMgmtRoutingModule
   ],
-  declarations: [BundleMgmtComponent, ConditionProductGroupComponent, ECatalogComponent, ItemListComponent, ProductDefinitionComponent]
+  declarations: [
+    ProductMgmtComponent,
+    ItemListComponent]
 })
 export class ProductMgmtModule { }
