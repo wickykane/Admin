8b7b74ecdd4c5a6e8fedd0e2432ae1beb2e37541
Khoa To <khoa.to@seldatinc.com>
2018-03-29 10:08:06 +0700

    sale price list

--- src/app/pages/order-mgmt/sale-price/sale-price-edit.component.ts
+++ src/app/pages/order-mgmt/sale-price/sale-price-edit.component.ts
@@ -5,6 +5,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { OrderService } from "../order-mgmt.service";
+import { ProductService } from "../../product-mgmt/product-mgmt.service";
 
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
@@ -13,90 +14,106 @@ import { ToastsManager } from 'ng2-toastr/ng2-toastr';
     selector: 'app-sale-price-edit',
     templateUrl: './sale-price-edit.component.html',
     styleUrls: ['./sale-price.component.scss'],
-    providers: [OrderService]
+    providers: [OrderService, ProductService]
 
 })
 
 export class SalePriceEditComponent implements OnInit {
-    /**
+     /**
      * Variable Declaration
      */
     public generalForm: FormGroup;
     public listMaster = {};
-    public data = {};
-    public all_customer_type = '1';
+    public data = {};    
     /**
      * Init Data
      */
-    constructor(private vRef: ViewContainerRef, private fb: FormBuilder, private orderService: OrderService, public toastr: ToastsManager, private router: Router, private modalService: NgbModal) {
+    constructor(private vRef: ViewContainerRef, private fb: FormBuilder, private orderService: OrderService,private productService:ProductService, public toastr: ToastsManager, private router: Router, private modalService: NgbModal,private route: ActivatedRoute) {
         this.toastr.setRootViewContainerRef(vRef);
         this.generalForm = fb.group({
             'cd': [{ value: null, disabled: true }],
-            'name': [null],
+            'des': [null],
             'sts': [null, Validators.required],
-            'apply_for': [null, Validators.required],
-            'crtd_on': [new Date(), Validators.required],
+            'start_dt': [null, Validators.required],
+            'end_dt': [null, Validators.required]            
         });
     }
 
     ngOnInit() {
-
-        this.data['segmentList'] = [];
-
+       
+        this.data['products'] = [];
         //Master Data
-      
+        this.listMaster['status'] = [
+            {
+                id: '0',
+                name: "In-Active"
+            }, {
+                id: '1',
+                name: "Active "
+            }
+        ];
+        this.data["id"] = this.route.snapshot.paramMap.get('id');
+        this.getDetailSalesPrice(this.data["id"]);
     }
+    getDetailSalesPrice(id) {
+        this.getListItemOption();
+        this.orderService.getDetailSalePrice(id).subscribe(res => {
+            try {
+                if (res._type == 'success') {
+                    this.generalForm.patchValue(res.results);
+                    this.data['products'] = res.results.products || [];
+                    this.data['products'].forEach((data, index) => {
+                        this.changeProductLine(data, index);
+                    });
+
+                } else {
+                    this.toastr.error(res.message);
+                }
+
+            } catch (e) {
+                console.log(e)
+            }
+        })
+
+    }
+    getListItemOption(){
+        this.productService.getListItemOption().subscribe(res=>{
+          this.listMaster['listProduct'] = res.results;     
+        })
+      }
 
     /**
      * Internal Function
      */
     clickAdd = function () {
-        this.data.segmentList.push({ 'has_specific_customer': false });
+        this.data.products.push({ });
     };
 
     remove = function (index) {
-        this.data.segmentList.splice(index, 1);
+        this.data.products.splice(index, 1);
     };    
 
-    changeToGetCustomer = function (segment, index) {
-
-        let params = { 'type': segment.type, 'country_code': segment.country_code }
-        Object.keys(params).forEach(function (item) {
-            if ((params[item]) instanceof Array) {
-                params[item] = params[item].join(',');
-            }
-            return item;
-        });
-
-        this.promotionService.getCustomerSegment(params).subscribe(res => {
-            try {
-                this.data.segmentList[index].customerList = res.results;
-            } catch (e) {
-                console.log(e);
-            }
+    changeProductLine(item,index){
+        let itemID = this.listMaster['listProduct'].filter(product=>{
+          if(product.item_id==item.wine_id)
+            return product;
         })
-    }
+        item.name= itemID[0].name;
+        item.uom_name=itemID[0].uom_name;
+        item.price=itemID[0].price;
+        item.resale_price =itemID[0].resale_price;
+      }
 
-    checkCustomerList = function (segment) {
-        setTimeout(() => {
-            if (!segment.customers) return;
-            if (segment.customers.length > 0) {
-                segment.has_specific_customer = true;
-            } else {
-                segment.has_specific_customer = false;
-            }
-        })
-    }
-
-    createSegment = function () {
+   
+    updateSalePrice = function () {
         let params = this.generalForm.value;
-        params.detail = Array.from( this.data.segmentList);
+        params.products = Array.from( this.data.products);
 
-        this.promotionService.postSegment(params).subscribe(res => {
+        this.orderService.updateSalePrice(this.data['id'],params).subscribe(res => {
             try {
                 this.toastr.success(res.message);
                 setTimeout(() => {
-                    this.router.navigate(['/customer/customer-segment']);
+                    this.router.navigate(['/order-management/sales-price']);
                 }, 500)
             } catch (e) {
                 console.log(e)
