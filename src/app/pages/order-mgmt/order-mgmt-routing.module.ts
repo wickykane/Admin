be769e9a04d8b6eb1442a0c4756d1603c92db378
Huy Dao <huy.dao@seldatinc.com>
2018-03-29 10:41:43 +0700

    list order

--- src/app/pages/order-mgmt/order-mgmt-routing.module.ts
+++ src/app/pages/order-mgmt/order-mgmt-routing.module.ts
@@ -1,7 +1,7 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import {OrderMgmtComponent} from './order-mgmt.component';
+import { OrderMgmtComponent } from './order-mgmt.component';
 import { BuyerRfqComponent } from './buyer-rfq/buyer-rfq.component';
 import { DeliveryOrderComponent } from './delivery-order/delivery-order.component';
 import { SaleOrderComponent } from './sale-order/sale-order.component';
@@ -9,13 +9,19 @@ import { SalePriceComponent } from './sale-price/sale-price.component';
 import { SaleQuotationComponent } from './sale-quotation/sale-quotation.component';
 
 
-const routes : Routes = [
+
+
+const routes: Routes = [
   {
     path: '',
     component: OrderMgmtComponent
   }, {
     path: 'buyer-rfq',
     component: BuyerRfqComponent
+  },
+  {
+    path: 'sale-order',
+    component: SaleOrderComponent
   }
 ];
 
@@ -23,4 +29,4 @@ const routes : Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class OrderMgmtRoutingModule {}
+export class OrderMgmtRoutingModule { }
--- src/app/pages/order-mgmt/order-mgmt.service.ts
+++ src/app/pages/order-mgmt/order-mgmt.service.ts
@@ -6,9 +6,78 @@ export class OrderService {
 
     constructor(private ApiService: ApiService) { }
     //Buyer RFQ
-    getListBuyerRFQ(params){
-        let url ='rfq';
-        return this.ApiService.get(url,params);
+    getListBuyerRFQ(params) {
+        let url = 'rfq';
+        return this.ApiService.get(url, params);
     }
 
+    //Order
+    getListOrder(params) {
+        let url = 'order/list-order';
+        return this.ApiService.get(url, params);
+    }
+
+    getListStatus() {
+        let url = 'order/list-status';
+        return this.ApiService.get(url);
+    }
+
+    countOrderStatus() {
+        let url = 'reports/order-status-count';
+        return this.ApiService.get(url);
+    }
+
+    getListItemById(params) {
+        var url = 'product/list-product-to-order';
+        return this.ApiService.get(url, params);
+    }
+
+    createOrder(params) {
+        var url = 'order/create-order';
+        return this.ApiService.post(url, params);
+    }
+
+    getOrderNumber(id) {
+        var url = ['order', 'generate-order-number', id].join('/');
+        return this.ApiService.get(url);
+    }
+
+    getStatisticalOrder(params) {
+        var url = 'order/statistical-order';
+        return this.ApiService.get(url, params);
+    }
+
+    approveOrd(ordId) {
+        var url = 'order/approved/' + ordId;
+        return this.ApiService.put(url);
+    }
+
+    cancelOrd(ordId) {
+        var url = 'order/cancel/' + ordId;
+        return this.ApiService.put(url);
+    }
+    sendEmail(params) {
+        var url = 'order/sent-message';
+        return this.ApiService.put(url, params);
+    }
+    generateSO() {
+        var url = 'order/get-order-code';
+        return this.ApiService.get(url);
+    }
+    getHistoryByCode(code) {
+        var url = "order/history?order_cd=" + code;
+        return this.ApiService.get(url);
+    }
+    getInvoice(id) {
+        var url = "order/invoice/" + id;
+        return this.ApiService.get(url);
+    }
+    previewOrder(params) {
+        var url = 'order/preview-order';
+        return this.ApiService.post(url, params);
+    }
+    cloneOrder(order_id) {
+        var url = 'order/clone-order/' + order_id;
+        return this.ApiService.post(url);
+    }
 }
