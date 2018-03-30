38f50459062ca4dd809c2dab2c56af17075bdaab
Khoa To <khoa.to@seldatinc.com>
2018-03-30 13:48:04 +0700

    update

--- src/app/pages/rma/rma.service.ts
+++ src/app/pages/rma/rma.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { ApiService } from '../../services/index';
 
 @Injectable()
 export class RmaService {
 
-  constructor() { }
+  constructor(private API: ApiService) {  }
+  getAllSts(params) {
+    let url = ['reports', 'get-all-orders-status'].join('/');
+    return this.API.get(url, params);
+  }
+
+  getRMAInfo() {
+    let url = ['rma', 'generate'].join('/');
+    return this.API.get(url);
+  }
+
+  getListOrders() {
+    let url = ['reference', 'order'].join('/');
+    return this.API.get(url);
+  }
+  getOrderById(id) {
+    let url = ['reference', 'order', id, 'items'].join('/');
+    return this.API.get(url);
+  }
+  createRMA(params) {
+    let url = ['rma', 'create-rma'].join('/');
+    return this.API.post(url, params);
+  }
 
 }
