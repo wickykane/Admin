86e8e3ef69d96ee6e49f84a80f3d78484e61c9d9
Khoa To <khoa.to@seldatinc.com>
2018-04-04 17:53:40 +0700

    update

--- src/environments/environment.qc.ts
+++ src/environments/environment.qc.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     api_url:  'https://apigw.seldatdirect.com/qc/sel2b-nab/api/api/v1/',
     url: 'http://nab.dev.seldatdirect.com/sel2b/',
     srs_url: "http://srs-haca.dev.seldatdirect.com/"
