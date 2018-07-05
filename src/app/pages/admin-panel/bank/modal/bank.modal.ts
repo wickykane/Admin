43892cb21437f000b472cd331b98309c05dc6e30
Huy Dao <huy.dao@seldatinc.com>
2018-07-05 12:56:55 +0700

    update bank

---
+++ src/app/pages/admin-panel/bank/modal/bank.modal.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BankService } from '../bank.service';
+
+@Component({
+  selector: 'app-bank-modal',
+  templateUrl: './bank.modal.html',
+  providers: [BankService]
+})
+export class BankModalComponent implements OnInit {
+  // Resolve Data
+  public bankForm: FormGroup;
+  @Input() item;
+
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private bankService: BankService) {
+    this.bankForm = fb.group({
+      'code': [null],
+      'name': [null],
+      'swift': [null],
+    });
+  }
+
+  ngOnInit() {
+    if (this.item.id) {
+      this.getDetailBank(this.item.id);
+    }
+  }
+
+  getDetailBank(id) {
+    this.bankService.getDetailBank(id).subscribe((res) => {
+      this.bankForm.patchValue(res.data);
+    });
+  }
+
+  ok() {
+    this.activeModal.close(this.bankForm.value);
+  }
+
+  cancel() {
+    this.activeModal.dismiss();
+  }
+}
---
+++ src/app/pages/admin-panel/bank/modal/branch.modal.ts
@@ -0,0 +1,68 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BankService } from '../bank.service';
+
+@Component({
+  selector: 'app-branch-modal',
+  templateUrl: './branch.modal.html',
+  providers: [BankService]
+})
+export class BranchModalComponent implements OnInit {
+  // Resolve Data
+  public branchForm: FormGroup;
+  public listMaster = {};
+  @Input() item;
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private bankService: BankService) {
+    this.branchForm = fb.group({
+      'bankname': [null],
+      'name': [null],
+      'country_code': [null],
+      'address': [null],
+      'city': [null],
+      'state_id': [null],
+      'zip_code': [null],
+    });
+  }
+
+  ngOnInit() {
+    this.branchForm.patchValue({ bankname: this.item['name'] });
+    this.getListCountry();
+  }
+
+  changeCountry() {
+    const id = this.branchForm.value.country_code;
+    const params = {
+      country: id
+    };
+    this.getStateByCountry(params);
+  }
+
+  getStateByCountry(params) {
+    this.bankService.getStateByCountry(params).subscribe(res => {
+      try {
+        this.listMaster['state'] = res.data;
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  }
+
+  getListCountry() {
+    this.bankService.getListCountry().subscribe(res => {
+      try {
+        this.listMaster['countries'] = res.data;
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  }
+
+  ok() {
+
+  }
+
+  cancel() {
+    this.activeModal.close();
+  }
+}
