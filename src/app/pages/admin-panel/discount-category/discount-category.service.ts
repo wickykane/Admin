c928b778bfb5d7c76f9f9b30baf4f58ae983a7e3
trung.le <trung.le@seldatinc.com>
2018-07-06 15:35:22 +0700

    update

--- src/app/pages/admin-panel/discount-category/discount-category.service.ts
+++ src/app/pages/admin-panel/discount-category/discount-category.service.ts
@@ -6,8 +6,17 @@ export class DiscountCategoryService {
     constructor(private API: ApiService) {}
 
     getListWarehouse(params) {
-        // const url = 'warehouse/index';
         const url = 'buyer/index';
         return this.API.get(url, params);
+    }
+
+    getListCategory() {
+        const url = 'category';
+        return this.API.get(url);
+    }
+
+    getListSubCategoryByCategory(id) {
+        const url = 'category/sub' + id;
+        return this.API.get(url);
     }
 }
