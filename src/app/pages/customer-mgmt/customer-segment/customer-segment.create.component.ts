5b1d0c286632880c176ffe1ca4aee3d6133694b4
trung.le <trung.le@seldatinc.com>
2018-07-24 12:11:42 +0700

    update

--- src/app/pages/customer-mgmt/customer-segment/customer-segment.create.component.ts
+++ src/app/pages/customer-mgmt/customer-segment/customer-segment.create.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbDateParserFormatter, NgbDateStruct, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateCustomParserFormatter } from '../../../shared/helper/dateformat';
 
 import { PromotionService } from '../../promotion-mgmt/promotion.service';
 import { CustomerService } from '../customer.service';
@@ -14,7 +14,7 @@ import { ToastrService } from 'ngx-toastr';
     selector: 'app-customer-segment',
     templateUrl: './customer-segment.create.component.html',
     styleUrls: ['./customer-segment.component.scss'],
-    providers: [PromotionService]
+    providers: [PromotionService, { provide: NgbDateParserFormatter, useClass: NgbDateCustomParserFormatter }]
 
 })
 
@@ -30,9 +30,9 @@ export class CustomerSegmentCreateComponent implements OnInit {
      * Init Data
      */
     constructor(private vRef: ViewContainerRef,
-         private fb: FormBuilder,
-          private promotionService: PromotionService,
-           private customerService: CustomerService, public toastr: ToastrService, private router: Router, private modalService: NgbModal) {
+        private fb: FormBuilder,
+        private promotionService: PromotionService,
+        private customerService: CustomerService, public toastr: ToastrService, private router: Router, private modalService: NgbModal) {
 
         this.generalForm = fb.group({
             'cd': [{ value: null, disabled: true }],
@@ -79,11 +79,11 @@ export class CustomerSegmentCreateComponent implements OnInit {
     /**
      * Internal Function
      */
-    clickAdd = function () {
+    clickAdd = function() {
         this.data.segmentList.push({ 'has_specific_customer': false });
     };
 
-    remove = function (index) {
+    remove = function(index) {
         this.data.segmentList.splice(index, 1);
     };
 
@@ -107,9 +107,9 @@ export class CustomerSegmentCreateComponent implements OnInit {
         });
     }
 
-    changeToGetCustomer  = (segment, index) => {
+    changeToGetCustomer = (segment, index) => {
         const params = { 'type': segment.type, 'country_code': segment.country_code };
-        Object.keys(params).forEach( (item) => {
+        Object.keys(params).forEach((item) => {
             if ((params[item]) instanceof Array) {
                 params[item] = params[item].join(',');
             }
@@ -131,9 +131,10 @@ export class CustomerSegmentCreateComponent implements OnInit {
         });
     }
 
-    createSegment = function () {
+    createSegment = function() {
         const params = this.generalForm.value;
-        params.detail = Array.from( this.data.segmentList);
+
+        params.detail = Array.from(this.data.segmentList);
 
         this.promotionService.postSegment(params).subscribe(res => {
             try {
@@ -146,7 +147,7 @@ export class CustomerSegmentCreateComponent implements OnInit {
             }
         },
             err => {
-                  this.toastr.error(err.message);
+                this.toastr.error(err.message);
             });
     };
 }
