a617b2d59e1a22bf9082fd9b6916826d397fdbc4
hiep tran <hiep.tran@seldatinc.com>
2018-08-08 09:47:15 +0700

    update pages

--- src/app/shared/modals/site.modal.ts
+++ src/app/shared/modals/site.modal.ts
@@ -181,9 +181,9 @@ export class SiteModalComponent implements OnInit, OnDestroy {
         }
     }
     changeBank(item) {
-        item.bank_swift = this.listBank.map(x => {
+         this.listBank.map(x => {
             if (item.bank_id === x.id) {
-                return x.swift;
+                item.bank_swift = x.swift;
             }
         });
 
