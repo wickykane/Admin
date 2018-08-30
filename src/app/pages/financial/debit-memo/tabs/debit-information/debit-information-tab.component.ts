4ba9be176f43bc3ff46a958f887f70a1579c46b9
Tu Chung <tu.chung@seldatinc.com>
2018-08-30 16:35:16 +0700

    Update View Debit Memo

--- src/app/pages/financial/debit-memo/tabs/debit-information/debit-information-tab.component.ts
+++ src/app/pages/financial/debit-memo/tabs/debit-information/debit-information-tab.component.ts
@@ -7,12 +7,13 @@ import { ToastrService } from 'ngx-toastr';
 import { ConfirmModalContent } from '../../../../../shared/modals/confirm.modal';
 import { TableService } from './../../../../../services/table.service';
 
+import { DebitMemoService } from '../../debit-memo.service';
 
 @Component({
     selector: 'app-debit-info-tab',
     templateUrl: './debit-information-tab.component.html',
     styleUrls: ['./debit-information-tab.component.scss'],
-    providers: []
+    providers: [DebitMemoService]
 })
 export class DebitInformationTabComponent implements OnInit {
 
@@ -33,7 +34,8 @@ export class DebitInformationTabComponent implements OnInit {
         private vRef: ViewContainerRef,
         private router: Router,
         private modalService: NgbModal,
-        public tableService: TableService) {
+        public tableService: TableService,
+        public debitService: DebitMemoService) {
     }
 
     ngOnInit() {
@@ -61,9 +63,56 @@ export class DebitInformationTabComponent implements OnInit {
         this.router.navigate(['/financial/debit-memo/edit',  this.debitData['id']]);
     }
 
-    onClickBack() {
-        window.history.back();
+    onChangeDebitStatus(newStatus) {
+        let modalMessage = '';
+        switch (newStatus) {
+            case 1: {
+                modalMessage = 'Are you sure that you want to reopen the current debit memo?';
+                break;
+            }
+            case 2: {
+                modalMessage = 'Are you sure that you want to submit this debit memo to approver?';
+                break;
+            }
+            case 3: {
+                modalMessage = 'Are you sure that you want to approve the current debit memo?';
+                break;
+            }
+            case 4: {
+                modalMessage = 'Are you sure that you want to reject the current debit memo?';
+                break;
+            }
+            case 5: {
+                modalMessage = 'Are you sure that you want to cancel current debit memo?';
+                break;
+            }
+        }
+        const modalRef = this.modalService.open(ConfirmModalContent);
+            modalRef.componentInstance.message = modalMessage;
+            modalRef.componentInstance.yesButtonText = 'YES';
+            modalRef.componentInstance.noButtonText = 'NO';
+            modalRef.result.then(yes => {
+                if (yes) {
+                    this.doUpdateDebitStatus(newStatus);
+                }
+            }, no => { });
     }
 
-    onUpdateDebit() {}
+    doUpdateDebitStatus(newStatus) {
+        this.debitService.updateDebitMemoStatus(this.debitData['id'], newStatus).subscribe(
+            res => {
+                try {
+                    this.toastr.success(res.message);
+                } catch (err) {
+                    console.log(err);
+                }
+            }, err => {
+                console.log(err);
+            }
+        );
+    }
+
+    onClickBack() {
+        this.router.navigate(['/financial/debit-memo']);
+    }
 }
