70c9f2411a0ba666e06d638700b6673b4b4a65a5
wickykane <huy.dao@seldatinc.com>
2018-09-20 17:01:41 +0700

    update key

--- src/app/pages/order-mgmt/sale-quotation/create/keys.create.control.ts
+++ src/app/pages/order-mgmt/sale-quotation/create/keys.create.control.ts
@@ -12,23 +12,7 @@ export class SaleQuoteCreateKeyService {
     private keyConfig = {
         company_id: {
             element: null,
-            prev: 'contact_user_id',
-            next: 'customer_po',
-        },
-        contact_user_id: {
-            element: null,
-            prev: 'company_id',
-            next: 'contact_user_id',
-        },
-        customer_po: {
-            element: null,
-            prev: 'company_id',
-            next: 'type',
-        },
-        type: {
-            element: null,
-            prev: 'customer_po',
-            next: 'type',
+            focus: true,
         },
     };
 
