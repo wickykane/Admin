c13708f8e3858aa661e213cec5f43740a1b2e8bd
trung.le <trung.le@seldatinc.com>
2018-10-10 10:56:17 +0700

    update

--- src/app/pages/order-mgmt/rma/modals/items-order.modal.ts
+++ src/app/pages/order-mgmt/rma/modals/items-order.modal.ts
@@ -1,22 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Hotkey, HotkeysService } from 'angular2-hotkeys';
 import { ToastrService } from 'ngx-toastr';
+import { cdArrowTable } from '../../../../shared/index';
+import { Helper } from './../../../../shared/helper/common.helper';
 
 import { RMAService } from '../rma.service';
+
+declare var jQuery: any;
 
 @Component({
     selector: 'app-items-order-modal-content',
     templateUrl: './items-order.modal.html',
     styleUrls: ['./items-order.modal.scss'],
-    providers: [RMAService]
+    providers: [RMAService, HotkeysService]
 })
 // tslint:disable-next-line:component-class-suffix
 export class ItemsOrderModalContent implements OnInit {
 
     public listIgnoredItems = [];
+    selectedIndex = 0;
     public orderId = '';
     @Input() set setIgnoredItems(data) {
         if (data && data['items'] && data['items'].length) {
@@ -28,19 +34,71 @@ export class ItemsOrderModalContent implements OnInit {
     public listItemSelected = [];
     public isCheckedAll = false;
     public searchForm: FormGroup;
+    @ViewChild(cdArrowTable) table: cdArrowTable;
 
     constructor(
         public activeModal: NgbActiveModal,
         public fb: FormBuilder,
         private toastr: ToastrService,
-        private service: RMAService) {
+        private service: RMAService,
+        private _hotkeysService: HotkeysService,
+        private helper: Helper) {
         this.searchForm = fb.group({
             no: [null],
             des: [null]
         });
     }
 
-    ngOnInit() {}
+    ngOnInit() {
+      this.initKeyBoard();
+    }
+
+    initKeyBoard() {
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+t', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            this.selectTable();
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Select Table'));
+
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+a', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            event.preventDefault();
+            this.onAddItem();
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Add'));
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+c', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            event.preventDefault();
+            this.activeModal.close();
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Close'));
+
+    }
+
+    resetKeys() {
+        const keys = Array.from(this._hotkeysService.hotkeys);
+        keys.map(key => {
+            this._hotkeysService.remove(key);
+        });
+    }
+
+
+    selectData(index) {
+      this.listItems[index].is_checked = !this.listItems[index].is_checked;
+      this.isAllChecked();
+    }
+
+    selectTable() {
+      this.selectedIndex = 0;
+      jQuery('.modal').focus();
+      this.table.scrollToTable('.modal-open .modal');
+    }
 
     onSearchList() {
         return this.listIgnoredItems.length && this.getListItems();
--- src/app/pages/product-mgmt/miscellaneous-items/keys.control.ts
+++ src/app/pages/product-mgmt/miscellaneous-items/keys.control.ts
@@ -6,6 +6,78 @@ import { Helper } from './../../../shared/helper/common.helper';
 
 @Injectable()
 export class MiscellaneousItemsKeyService extends KeyboardBaseService {
+  keyConfig = {
+      no: {
+          element: null,
+          focus: true,
+      }
+  };
 
+  initKey() {
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+f', (event: KeyboardEvent): boolean => {
+          event.preventDefault();
+          if (this.keyConfig.no.element) {
+              this.keyConfig.no.element.nativeElement.focus();
+          }
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Focus Search'));
 
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+n', (event: KeyboardEvent): boolean => {
+          event.preventDefault();
+          this.context.openModal(true, false);
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Create Miscellaneous'));
+
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+e', (event: KeyboardEvent): boolean => {
+          event.preventDefault();
+          this.context.tableService.searchAction();
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Search'));
+
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+r', (event: KeyboardEvent): boolean => {
+          event.preventDefault();
+          this.context.tableService.resetAction(this.context.searchForm);
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Reset'));
+
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+v', (event: KeyboardEvent): boolean => {
+          event.preventDefault();
+          const item = this.context.list['items'][this.context.selectedIndex];
+          this.context.openModal(false, true, item);
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'View'));
+
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+e', (event: KeyboardEvent): boolean => {
+          event.preventDefault();
+          const item = this.context.list['items'][this.context.selectedIndex];
+          this.context.openModal(false, false, item);
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Edit'));
+
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+pageup', (event: KeyboardEvent): boolean => {
+          this.context.tableService.pagination.page--;
+          if (this.context.tableService.pagination.page < 1) {
+              this.context.tableService.pagination.page = 1;
+              return;
+          }
+          this.context.tableService.changePage(this.context.tableService.pagination.page);
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Next page'));
+
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+pagedown', (event: KeyboardEvent): boolean => {
+          this.context.tableService.pagination.page++;
+          if (this.context.tableService.pagination.page > this.context.tableService.pagination.total_page) {
+              this.context.tableService.pagination.page = this.context.tableService.pagination.total_page;
+              return;
+          }
+          this.context.tableService.changePage(this.context.tableService.pagination.page);
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Previous page'));
+
+      this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+t', (event: KeyboardEvent): boolean => {
+          event.preventDefault();
+          this.context.selectTable();
+          return;
+      }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Select Table'));
+  }
 }
