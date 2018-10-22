c88d3cb41f1b65e85229ffb7be5e61ca478b7e38
Tu Chung <tu.chung@seldatinc.com>
2018-10-22 11:29:29 +0700

    Add key shortcut Carrier Create

--- src/app/pages/admin-panel/carrier/create/keys.control.ts
+++ src/app/pages/admin-panel/carrier/create/keys.control.ts
@@ -1,48 +1,108 @@
-import { Injectable, OnDestroy } from '@angular/core';
-import { Hotkey, HotkeysService } from 'angular2-hotkeys';
-// tslint:disable-next-line:import-blacklist
-import { Subject } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
+import { Hotkey } from 'angular2-hotkeys';
+import { KeyboardBaseService } from './../../../../shared/helper/keyServiceBase';
 @Injectable()
-export class CarrierCreateKeyService implements OnDestroy {
-    public context: any;
-    public watchContext = new Subject<any>();
-
-    constructor(private _hotkeysService: HotkeysService) {
-        this.watchContext.subscribe(res => {
-            this.context = res;
-            this.initKey();
-        });
-    }
-
-    ngOnDestroy() {
-        this.resetKeys();
-    }
-
-    resetKeys() {
-        const keys = this.getKeys();
-        for (const key of keys) {
-            this._hotkeysService.remove(key);
+export class CarrierCreateKeyService extends KeyboardBaseService {
+    keyConfig = {
+        name: {
+            element: null,
+            focus: false,
+        },
+        primary_email: {
+            element: null,
+            focus: false,
+        },
+        billing_email: {
+            element: null,
+            focus: false,
+        },
+        back_button: {
+            element: null,
+            focus: true,
         }
-    }
-
-    getKeys() {
-        return Array.from(this._hotkeysService.hotkeys);
-    }
+    };
 
     initKey() {
-        this.resetKeys();
-        this._hotkeysService.add(new Hotkey('ctrl+f', (event: KeyboardEvent): any => {
-            event.preventDefault();
-            this.context.tableService.searchAction();
-            event.returnValue = false;
-            return event;
-        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Search'));
-        this._hotkeysService.add(new Hotkey('ctrl+r', (event: KeyboardEvent): any => {
-            event.preventDefault();
-            this.context.tableService.resetAction(this.context.searchForm);
-            event.returnValue = false;
-            return event;
-        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Reset'));
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+backspace', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            (document.activeElement as HTMLElement).blur();
+            this.context.router.navigate(['/admin-panel/carrier']);
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Back'));
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+f1', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            (document.activeElement as HTMLElement).blur();
+            if (this.keyConfig.name.element) {
+                this.keyConfig.name.element.nativeElement.focus();
+            }
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Focus General'));
 
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+f2', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            (document.activeElement as HTMLElement).blur();
+            if (this.keyConfig.primary_email.element) {
+                this.keyConfig.primary_email.element.nativeElement.focus();
+            }
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Focus Primary'));
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+f3', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            (document.activeElement as HTMLElement).blur();
+            if (this.keyConfig.billing_email.element) {
+                this.keyConfig.billing_email.element.nativeElement.focus();
+            }
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Focus Billing'));
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+t', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            if (this.context.listBankAccount.length) {
+                this.context.selectTable();
+            }
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Select Table'));
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+i', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            (document.activeElement as HTMLInputElement).blur();
+            this.context.addNewBank();
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Add new Line'));
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+del', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            const item = this.context.listBankAccount[this.context.selectedIndex];
+            this.context.removeBankAccount(this.context.selectedIndex, item);
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Delete Item'));
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+s', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            (document.activeElement as HTMLElement).blur();
+            if (this.context.valid()) {
+                this.context.confirm();
+            }
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Save'));
+
+        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+c', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+            this.context.copyAddress();
+            if (this.keyConfig.billing_email.element) {
+                this.keyConfig.billing_email.element.nativeElement.focus();
+            }
+            const e: ExtendedKeyboardEvent = event;
+            e.returnValue = false; // Prevent bubbling
+            return e;
+        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Copy to Billing Address'));
     }
 }
