f57883813d6292bd175205397486d1f22755ba08
Tu Chung <tu.chung@seldatinc.com>
2018-10-31 14:54:44 +0700

    [NABPRO-3310] - Update UI Tab Receipt Voucher Customer View

--- src/app/pages/customer-mgmt/customer/information-tab/receipt-voucher/receipt-voucher-tab.component.ts
+++ src/app/pages/customer-mgmt/customer/information-tab/receipt-voucher/receipt-voucher-tab.component.ts
@@ -7,11 +7,13 @@ import { CustomerService } from '../../../customer.service';
 import { CustomerKeyViewService } from '../../view/keys.view.control';
 import { TableService } from './../../../../../services/table.service';
 
+import { ReceiptVoucherService } from '../../../../financial/receipt-voucher/receipt-voucher.service';
+
 @Component({
     selector: 'app-customer-receipt-voucher-tab',
     templateUrl: './receipt-voucher-tab.component.html',
     styleUrls: ['../information-tab.component.scss'],
-    providers: [HotkeysService, CustomerKeyViewService],
+    providers: [HotkeysService, CustomerKeyViewService, ReceiptVoucherService],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
@@ -45,6 +47,7 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
         public tableService: TableService,
         public _hotkeysServiceReceipt: HotkeysService,
         private helper: Helper,
+        private receiptVoucherService: ReceiptVoucherService,
         private cd: ChangeDetectorRef) {
 
         this.searchForm = fb.group({
@@ -65,7 +68,18 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-      this.getList();
+        this.listMaster['dateType'] = [
+            { id: 'payment_date', name: 'Payment Date' },
+            { id: 'created_at', name: 'Created On' },
+            { id: 'updated_at', name: 'Updated On' }
+        ];
+        this.listMaster['electType'] = [
+            { id: 0, name: 'No' },
+            { id: 1, name: 'Yes' }
+        ];
+        this.getListStatus();
+        this.getListReferenceData();
+        this.getList();
     }
     selectData(index) {
         console.log(index);
@@ -73,17 +87,17 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
     /**
      * Internal Function
      */
-     refresh() {
-          if (!this.cd['destroyed']) { this.cd.detectChanges(); }
-     }
+    refresh() {
+        if (!this.cd['destroyed']) { this.cd.detectChanges(); }
+    }
 
     getList() {
         const params = {...this.tableService.getParams(), ...this.searchForm.value};
         Object.keys(params).forEach((key) => (params[key] === null || params[key] ===  '') && delete params[key]);
 
-        this.customerService.getListInvoice(this._customerId, params).subscribe(res => {
+        this.customerService.getListPayment(this._customerId, params).subscribe(res => {
             try {
-                this.list.items = [] || res.data.rows;
+                this.list.items = res.data.rows || [];
                 this.tableService.matchPagingOption(res.data);
                 this.refresh();
             } catch (e) {
@@ -91,10 +105,27 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
             }
         });
     }
+
+    getListStatus() {
+        this.receiptVoucherService.countVoucherStatus().subscribe(res => {
+            this.listMaster['list-status'] = res.data;
+            this.refresh();
+        });
+    }
+
+    getListReferenceData() {
+        this.receiptVoucherService.getPaymentMethodOption().subscribe(res => {
+            this.listMaster['paymentMethod'] = res.data.payment_method;
+            this.refresh();
+        });
+    }
+
     selectTable() {
         this.selectedIndex = 0;
-        this.table.element.nativeElement.querySelector('td').focus();
+        this.table.element.nativeElement.querySelector('td a').focus();
+        this.refresh();
     }
+
     initKeyBoard() {
         this.data['key_config'] = {
             receipt_no: {
@@ -102,12 +133,6 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
                 focus: true,
             },
         };
-        // saveKeys() {
-        //     this.keys = this.getKeys();
-        //     this.context.data['tableKey'] = this.context.table.getKeys();
-        //     this.resetKeys();
-        //     this.context.table.resetKeys();
-        // }
 
         this._hotkeysServiceReceipt.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+f1', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
             event.preventDefault();
@@ -125,6 +150,7 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
             return;
         }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Search'));
         this._hotkeysServiceReceipt.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+t', (event: KeyboardEvent): boolean => {
+            (document.activeElement as HTMLInputElement).blur();
             event.preventDefault();
             this.selectTable();
             return;
@@ -144,7 +170,7 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
             return;
         }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Previous page'));
 
-        this._hotkeysServiceReceipt.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+d', (event: KeyboardEvent): boolean => {
+        this._hotkeysServiceReceipt.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+y', (event: KeyboardEvent): boolean => {
             this.tableService.pagination.page++;
             if (this.tableService.pagination.page > this.tableService.pagination.total_page) {
                 this.tableService.pagination.page = this.tableService.pagination.total_page;
@@ -154,6 +180,7 @@ export class CustomerReceiptVoucherTabComponent implements OnInit, OnDestroy {
             return;
         }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Next page'));
     }
+
     resetKeys() {
         const keys = Array.from(this._hotkeysServiceReceipt.hotkeys);
         keys.map(key => {
