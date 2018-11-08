4e64a469d8cf96b387ce962a94ad5a04c8a6f35e
Tu Chung <tu.chung@seldatinc.com>
2018-11-08 16:41:16 +0700

    [NABPRO-3546] - Fix permission Create/Edit Sales Order

--- src/app/pages/order-mgmt/sale-order/create/keys.control.ts
+++ src/app/pages/order-mgmt/sale-order/create/keys.control.ts
@@ -73,15 +73,17 @@ export class SaleOrderCreateKeyService extends KeyboardBaseService {
             return e;
         }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Save as Draft'));
 
-        this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+v', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
-            (document.activeElement as HTMLElement).blur();
-            if (this.context.generalForm.valid) {
-                this.context.confirmCreateOrder('validate');
-            }
-            const e: ExtendedKeyboardEvent = event;
-            e.returnValue = false; // Prevent bubbling
-            return e;
-        }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Save & Validate'));
+        if (this.context.listMaster['permission'].approve) {
+            this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+v', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
+                (document.activeElement as HTMLElement).blur();
+                if (this.context.generalForm.valid) {
+                    this.context.confirmCreateOrder('validate');
+                }
+                const e: ExtendedKeyboardEvent = event;
+                e.returnValue = false; // Prevent bubbling
+                return e;
+            }, ['INPUT', 'SELECT', 'TEXTAREA'], 'Save & Validate'));
+        }
 
         this._hotkeysService.add(new Hotkey(`${this.helper.keyBoardConst()}` + '+s', (event: KeyboardEvent, combo: string): ExtendedKeyboardEvent => {
             (document.activeElement as HTMLElement).blur();
